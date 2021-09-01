770d596cf4f7e90545305d8079c21a5a3e9a3d44
Somon Das <somondas067@gmail.com>
2021-08-31 20:34:13 -0700

    init

---
+++ sub-pages/script/script.js
@@ -0,0 +1,12 @@
+function minimize(){
+    var x = document.getElementById("min")
+    if(x.style.display === "none"){
+        x.style.display = "block"
+    }else{
+        x.style.display = "none"
+    }
+    
+}
+
+
+
